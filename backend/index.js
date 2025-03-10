1344f9685ac43e52e7f1553c07b4d61e145c2cbc
Ankit-Yadav <ankityadav89181948@gmail.com>
2025-03-10 23:44:47 +0530

    added url

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ var server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5174",
+    origin: "https://chat-club-client.onrender.com",,
     methods: ["GET", "POST"]
   }
 });
